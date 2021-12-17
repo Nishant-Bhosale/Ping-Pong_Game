35b9e2c59f2de6d3420f788ce08c04b1cc455ccf
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-12-17 17:43:07 +0530

    Added ball class

--- Ball.js
+++ Ball.js
@@ -3,7 +3,24 @@ export default class Ball{
       this.ballElem = ballElement;
    }
 
-   function update(){
-      
+   get x(){
+      return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+   }
+
+   set x(value){
+      this.ballElem.style.setProperty("--x", value);
+   }
+
+   get y(){
+      return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+   }
+
+   set y(value){
+      this.ballElem.style.setProperty("--y", value);
+   }
+
+   update(delta){
+      this.x = 5;
+      this.y = 15;
    }
 }
